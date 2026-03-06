c39f1f86c431f3503bf87071156413123d9d23e5
atsstats <atsstats@proton.me>
2026-03-05 20:02:51 -0700

    Finished Third Project with Format Specifiers

--- Third Project/main.c
+++ Third Project/main.c
@@ -3,7 +3,6 @@
 // Format Specifier = Special tokens that begin with a % symbol followed by a character that specifies the data type and optional modifiers(width, precision, flags). 
 //                      They control how data is displayed or interpreted.
 
-
 int main() {
 
     int age = 23;
@@ -11,12 +10,28 @@ int main() {
     double pi = 3.1415926535;
     char currency = '$';
     char name[] = "Atsstats";
+    int num1 = 1;
+    int num2 = 10;
+    int num3 = -100;
+    float price1 = 19.99;
+    float price2 = 1.50;
+    float price3 = -100.00;
+
+
+
+
 
     printf("%d\n", age);
     printf("%.2f\n", price);
     printf("%lf\n", pi);
-
-
+    printf("%c\n", currency);
+    printf("%s\n", name);
+    printf("%-4d\n", num1);
+    printf("%-4d\n", num2);
+    printf("%+-4d\n", num3);
+    printf("%+7.2f\n", price1);
+    printf("%+7.2f\n", price2);
+    printf("%+7.2f\n", price3);
 
 
 
